64646a0c9b9f4fd43b957ef98d7b8394ba6f2b35
Bauyrzhan Ospan <dr.cleverest@gmail.com>
2019-08-26 16:30:12 +0000

    Changed install

--- bot/install.sh
+++ bot/install.sh
@@ -6,10 +6,10 @@ echo "Copied control.conf"
 cp ./flask.service /lib/systemd/system/flask.service
 cp ./bot.service /lib/systemd/system/bot.service
 echo "Added all files"
-chown $USER flask.py
+chown $USER flasik.py
 chown $USER main.py
 chmod 777 main.py
-chmod 777 flask.py
+chmod 777 flasik.py
 sudo systemctl daemon-reload
 echo "Done chown and chmod"
 sudo service bot restart && sudo service flask restart
